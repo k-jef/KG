7417278851424575f0a3ff9bfbee247e13d3e2c0
k-jef <80980646+k-jef@users.noreply.github.com>
2023-02-21 09:22:41 +0300

    Add files via upload

---
+++ Lab1_Lab1_Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        Bitmap image;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image files | *.png; *.jpg; *.bmp | Alll Files (*.*) | *.*";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                image = new Bitmap(dialog.FileName);
+            }
+            pictureBox1.Image = image;
+            pictureBox1.Refresh();
+        }
+
+        private void инверсияToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            InvertFilter filter = new InvertFilter();
+            Bitmap resultImage = filter.processImage(image);
+            pictureBox1.Image = resultImage;
+            pictureBox1.Refresh();
+        }
+
+        private void мToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Contrast filter = new Contrast();
+            filter.Ravg = filter.AvgR(image);
+            filter.Gavg = filter.AvgG(image);
+            filter.Bavg = filter.AvgB(image);
+            filter.Avg = (filter.Ravg + filter.Gavg + filter.Bavg) / 3;
+
+
+            Bitmap resultImage = filter.processImage(image);
+            pictureBox1.Image = resultImage;
+            pictureBox1.Refresh();
+        }
+
+        private void резкостьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Filters filter = new Sharpness();
+            Bitmap resultImage = filter.processImage(image);
+            pictureBox1.Image = resultImage;
+            pictureBox1.Refresh();
+        }
+    }
+}
